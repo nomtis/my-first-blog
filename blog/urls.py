a049759adeaa0c6bc6adb87b679fd5c70dfd820d
Your Name <you@example.com>
2019-06-19 15:33:30 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 15:07:24 2019
+
+@author: dnsusr04
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
